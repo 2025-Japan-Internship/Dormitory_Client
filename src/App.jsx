28ddbfb331ff2f7ee241f5a518747bcf6545477d
GitJiYeon <s2436@e-mirim.hs.kr>
2025-12-16 08:28:01 +0900

    배포 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
-import { HashRouter, Routes, Route } from 'react-router-dom';
-// BrowserRouter 대신 HashRouter 사용
+import { Routes, Route } from 'react-router-dom';
+// BrowserRouter 제거
+import Song from './pages/Song';
+import Home from './pages/Home';
+import Meal from './pages/Meal';
+import Login from "./pages/Login";
+import SuggestionPage from './pages/SuggestionPage';
+import SuggestionWritePage from './pages/SuggestionWritePage';
+import NoticeList from "./pages/noticeList";
+import NoticeDetail from "./pages/noticeDetail";
+import SelectRoom from "./pages/SelectRoom";
+import ScanQR from "./pages/scanQR";
 
 const App = () => {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/song" element={<Song />} />
-        <Route path="/meal" element={<Meal />} />
-        <Route path="/" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/selectRoom" element={<SelectRoom />} />
-        <Route path="/suggestion" element={<SuggestionPage />} />
-        <Route path="/suggestion/write" element={<SuggestionWritePage />} />
-        <Route path="/noticeList" element={<NoticeList />} />
-        <Route path="/notices/:id" element={<NoticeDetail />} />
-        <Route path="/scanQR" element={<ScanQR />} />
-      </Routes>
-    </HashRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/song" element={<Song />} />
+      <Route path="/meal" element={<Meal />} />
+      <Route path="/selectRoom" element={<SelectRoom />} />
+      <Route path="/suggestion" element={<SuggestionPage />} />
+      <Route path="/suggestion/write" element={<SuggestionWritePage />} />
+      <Route path="/noticeList" element={<NoticeList />} />
+      <Route path="/notices/:id" element={<NoticeDetail />} />
+      <Route path="/scanQR" element={<ScanQR />} />
+    </Routes>
   );
 };
 
